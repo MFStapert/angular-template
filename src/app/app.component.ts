4e7a083e4fff6aa83815b45316ba7c2e33ff3ddf
Marijn Stapert <mf.stapert@gmail.com>
2022-01-22 20:22:12 +0100

    add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1 class="home">
+      Hello world!
+    </h1>
+  `,
+  styles: [`
+    .home {
+      @apply text-3xl font-bold underline;
+    }`
+  ]
 })
-export class AppComponent {
-  title = 'angular-template';
-}
+export class AppComponent {}
