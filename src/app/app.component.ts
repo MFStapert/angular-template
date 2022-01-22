1ffe6309ed618d15cf65437fdf02e09445e8aabf
Marijn Stapert <mf.stapert@gmail.com>
2022-01-22 20:36:56 +0100

    prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1 class="home">
-      Hello world!
-    </h1>
-  `,
-  styles: [`
-    .home {
-      @apply text-3xl font-bold underline;
-    }`
-  ]
+  template: ` <h1 class="home">Hello world!</h1> `,
+  styles: [
+    `
+      .home {
+        @apply text-3xl font-bold underline;
+      }
+    `,
+  ],
 })
 export class AppComponent {}
